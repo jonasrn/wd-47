4d14caf59910be4da3998327bdddab903d56aab7
jonas nepomuceno <jonasrn@gmail.com>
2015-05-14 16:00:23 -0300

    Criando exemplo mobile

--- loja/js/modules/moeda.js
+++ loja/js/modules/moeda.js
@@ -9,6 +9,8 @@ function realParaNumber(texto) {
 }
 
 */
+
+/*
 var formatadorMoeda = {
 		simbolo: "R$ ",
 		numberParaReal:function (numero) {
@@ -21,3 +23,25 @@ var formatadorMoeda = {
 		
 		
 }
+*/
+
+var formatadorMoeda = (function(){
+	"use strict";
+	 var simbolo = "R$ ";
+	
+	return {
+		
+			numberParaReal:function(numero) {
+				return simbolo + numero.toFixed(2).replace(".", ",");
+			},
+			
+			realParaNumber:function (texto) {
+				return parseFloat(texto.replace(simbolo, "").replace(",", "."));
+			}
+		
+		};
+		
+	
+	
+})();
+
--- loja/js/sugestoes/sugestoes.js
+++ loja/js/sugestoes/sugestoes.js
@@ -18,3 +18,27 @@ $("input[type=button]").click(function(event){
 $(".sugestoes").on("dblclick","li",function(event) {
 	$(this).remove();
 });
+
+
+$("input[type=text]").autocomplete({
+	source: function(entrada, responsta) {
+		var sugestoesParecidas = [];
+		
+		var novaSugestao = new RegExp(entrada.term, "i");
+		
+		$(".sugestao").each(function(){
+			var sugestaoExistente = $(this).text();
+			
+			if(sugestaoExistente.match(novaSugestao)){
+				 sugestoesParecidas.push(sugestaoExistente);
+			}
+		});
+		
+		responsta(sugestoesParecidas);
+		
+		
+	}
+	
+	
+});
+
