8b893b75e0994d0253e890c41605638a116e44ea
jonas nepomuceno <jonasrn@gmail.com>
2015-05-14 16:00:51 -0300

    Criando exemplo mobile 2

---
+++ loja/js/pesquisa/swipe.js
@@ -0,0 +1,31 @@
+(function($) {
+	"use strict";
+	
+	var sacola = $(".sacola");
+	
+	if(!sacola.data("total")){
+		sacola.data("total", 0);
+	}
+	
+	$("#carrinho li").on("swipeleft", function(event) {
+		
+		var li = $(this);
+		
+		li.fadeOut("slow", function(){
+			var total = sacola.data("total") +1;
+			
+			sacola.data("total", total);
+			
+			sacola.text("Você tem" + total + "produto(s) sacolas");
+			
+			$(this).remove();
+			
+			
+			
+		});
+		
+		
+	});
+	
+	
+})(jQuery);
